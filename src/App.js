805d55dad4e51eb2436151f8dfac62cae58e8f63
Kyle Caprio <capriok7@yahoo.com>
2020-01-24 17:48:02 -0700

    touches

--- src/App.js
+++ src/App.js
@@ -1,97 +1,97 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { StateProvider } from './state'
-import Navbar from './components/Navbar/navbar.js'
-import Newsfeed from './components/Newsfeed/newsfeed.js'
-import Profile from './components/Profile/profile.js'
-import './Main.css'
+import React, { useState, useEffect } from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { StateProvider } from "./state"
+import Navbar from "./components/Navbar/navbar.js"
+import Newsfeed from "./components/Newsfeed/newsfeed.js"
+import Profile from "./components/Profile/profile.js"
+import "./Main.css"
 
 export default function App() {
-	const initialState = {
-		auth: {
-			isAuthenticated: false,
-			token: '',
-			user: {}
-		},
-		endpoint: {
-			everything: false,
-			headlines: false,
-			sources: false
-		},
-		options: {
-			q: '',
-			sources: '',
-			category: '',
-			country: '',
-			to: '',
-			from: '',
-			language: '',
-		}
-	}
+  const initialState = {
+    auth: {
+      isAuthenticated: false,
+      token: "",
+      user: {}
+    },
+    endpoint: {
+      everything: false,
+      headlines: false,
+      sources: false
+    },
+    options: {
+      q: "",
+      sources: "",
+      category: "",
+      country: "",
+      to: "",
+      from: "",
+      language: ""
+    }
+  }
 
-	const reducer = (state, action) => {
-		switch (action.type) {
-			case 'login':
-				return {
-					...state,
-					auth: action.auth
-				}
-			case 'logout':
-				return {
-					...initialState.auth
-				}
-			case 'filter':
-				return {
-					...state,
-					endpoint: action.endpoint
-				}
-			case 'pass':
-				return {
-					...state,
-					options: action.options
-				}
-			default:
-				return state
-		}
-	}
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "login":
+        return {
+          ...state,
+          auth: action.auth
+        }
+      case "logout":
+        return {
+          ...initialState.auth
+        }
+      case "filter":
+        return {
+          ...state,
+          endpoint: action.endpoint
+        }
+      case "pass":
+        return {
+          ...state,
+          options: action.options
+        }
+      default:
+        return state
+    }
+  }
 
-	const [feedOptionsOpen, setFeedOptionsOpen] = useState(false)
+  const [feedOptionsOpen, setFeedOptionsOpen] = useState(false)
 
-	const toggleFeedOptions = () => {
-		setFeedOptionsOpen(!feedOptionsOpen)
-		if (!feedOptionsOpen) {
-			window.scrollTo(0, 0)
-		}
-	}
+  const toggleFeedOptions = () => {
+    setFeedOptionsOpen(!feedOptionsOpen)
+    if (!feedOptionsOpen) {
+      window.scrollTo(0, 0)
+    }
+  }
 
-	useEffect(() => {
-		let authorize = localStorage.getItem('TF-token')
-		if (authorize) {
-			initialState.auth.isAuthenticated = true
-		}
-		console.log('Auth Status', initialState.auth.isAuthenticated);
-		console.log('Welcome to TookFeed')
-	}, [initialState.auth.isAuthenticated])
+  useEffect(() => {
+    let authorize = localStorage.getItem("TF-token")
+    if (authorize) {
+      initialState.auth.isAuthenticated = true
+    }
+    console.log("Auth Status =>", initialState.auth.isAuthenticated)
+    console.log("Welcome to TookFeed")
+  }, [initialState.auth.isAuthenticated])
 
-	return (
-		<StateProvider initialState={initialState} reducer={reducer}>
-			<Navbar toggleFO={toggleFeedOptions} />
-			<div className='container'>
-				<Router>
-					<Switch>
-						<Route
-							exact
-							path='/'
-							render={() => (
-								<>
-									<Newsfeed feedOptionsOpen={feedOptionsOpen} />
-								</>
-							)}
-						/>
-						<Route path='/profile' component={Profile} />
-					</Switch>
-				</Router>
-			</div>
-		</StateProvider>
-	)
+  return (
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Navbar toggleFO={toggleFeedOptions} />
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <>
+                  <Newsfeed feedOptionsOpen={feedOptionsOpen} />
+                </>
+              )}
+            />
+            <Route path="/profile" component={Profile} />
+          </Switch>
+        </Router>
+      </div>
+    </StateProvider>
+  )
 }
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,87 +1,93 @@
-import React, { useState, useEffect } from 'react'
-import { useStateValue } from '../../state'
-import './navbar.css'
-import Logo from './img/logo.png'
-import Feeds from './img/navfeeds.png'
-import Profile from './img/navprofile.png'
-import Logout from './img/navlogout.png'
-import AccModal from '../AccModal/accmodal.js'
+import React, { useState, useEffect } from "react"
+import { useStateValue } from "../../state"
+import "./navbar.css"
+import Logo from "./img/logo.png"
+import Feeds from "./img/navfeeds.png"
+import Profile from "./img/navprofile.png"
+import Logout from "./img/navlogout.png"
+import AccModal from "../AccModal/accmodal.js"
 
 function Navbar({ toggleFO }) {
-	const [accModalOpen, setAccModalOpen] = useState(false)
-	const [{ auth }, dispatch] = useStateValue()
+  const [accModalOpen, setAccModalOpen] = useState(false)
+  const [{ auth }, dispatch] = useStateValue()
 
-	const toggleAccModal = () => setAccModalOpen(!accModalOpen)
+  const toggleAccModal = () => setAccModalOpen(!accModalOpen)
 
-	const Clickout = () => (
-		<div onClick={toggleAccModal} className='clickout'></div>
-	)
+  const Clickout = () => (
+    <div onClick={toggleAccModal} className="clickout"></div>
+  )
 
-	const LoginRegsterButton = props => (
-		<button className='accountbtn' onClick={toggleAccModal}>Login | Register</button>
-	)
+  const LoginRegsterButton = props => (
+    <button className="accountbtn" onClick={toggleAccModal}>
+      Login | Register
+    </button>
+  )
 
-	const FeedOptionsButton = () => (
-		<img className='navicon' onClick={toggleFO} src={Feeds} alt=""></img>
-	)
+  const FeedOptionsButton = () => (
+    <img className="navicon" onClick={toggleFO} src={Feeds} alt=""></img>
+  )
 
-	const ProfileButton = () => (
-		<a href="/profile"><img className='navicon' src={Profile} alt=""></img></a>
-	)
+  const ProfileButton = () => (
+    <a href="/profile">
+      <img className="navicon" src={Profile} alt=""></img>
+    </a>
+  )
 
-	const LogoutButton = () => (
-		<img className='navicon' onClick={logoutUser} src={Logout} alt=""></img>
-	)
+  const LogoutButton = () => (
+    <img className="navicon" onClick={logoutUser} src={Logout} alt=""></img>
+  )
 
+  const logoutUser = () => {
+    localStorage.removeItem("token")
+    localStorage.removeItem("user")
+    window.location.href = "/"
+    dispatch({
+      type: "logout"
+    })
+  }
 
-	const logoutUser = () => {
-		localStorage.removeItem('token')
-		localStorage.removeItem('user')
-		window.location.href = '/'
-		dispatch({
-			type: 'logout'
-		})
-	}
+  useEffect(() => {
+    const token = localStorage.getItem("TF-token")
+    const user = localStorage.getItem("TF-user")
+    if (token !== null) {
+      dispatch({
+        type: "login",
+        auth: {
+          isAuthenticated: true,
+          token: token,
+          user: JSON.parse(user)
+        }
+      })
+    }
+    console.log(token)
+  }, [dispatch])
 
-	useEffect(() => {
-		const token = localStorage.getItem('TF-token')
-		const user = localStorage.getItem('TF-user')
-		if (token !== null) {
-			dispatch({
-				type: 'login',
-				auth: {
-					isAuthenticated: true,
-					token: token,
-					user: JSON.parse(user)
-				}
-			})
-		}
-	}, [dispatch])
-
-	return (
-		<>
-			{accModalOpen && <Clickout onClick={toggleAccModal} />}
-			{accModalOpen && <AccModal toggleAccModal={toggleAccModal} />}
-			<header>
-				<nav>
-					<div className='navlogo'>
-						<a href="/"><img className='logo' src={Logo} alt='img' /></a>
-					</div>
-					<div>
-						{auth.isAuthenticated ? (
-							<>
-								<FeedOptionsButton />
-								<ProfileButton />
-								<LogoutButton />
-							</>
-						) : (
-								<LoginRegsterButton />
-							)}
-					</div>
-				</nav>
-			</header>
-		</>
-	)
+  return (
+    <>
+      {accModalOpen && <Clickout onClick={toggleAccModal} />}
+      {accModalOpen && <AccModal toggleAccModal={toggleAccModal} />}
+      <header>
+        <nav>
+          <div className="navlogo">
+            <a href="/">
+              <img className="logo" src={Logo} alt="img" />
+            </a>
+          </div>
+          <div>
+            {auth.isAuthenticated ? (
+              <>
+                <FeedOptionsButton />
+                <ProfileButton />
+                <LogoutButton />
+              </>
+            ) : (
+              <LoginRegsterButton />
+            )}
+          </div>
+        </nav>
+      </header>
+    </>
+  )
 }
 
 export default Navbar
